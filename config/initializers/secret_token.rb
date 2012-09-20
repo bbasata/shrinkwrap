7f95cb7d9a410475bbd33ae4ac112ddbca346b5e
Baraa Basata <bbasata@gmail.com>
2012-09-19 22:02:13 -0400

    rails new shrinkwrap --skip-test-unit --skip-javascript --skip-sprockets --skip-active-record

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shrinkwrap::Application.config.secret_token = 'b43f937ef5c32b9901064a177c3314818124c6c7506d9c1f0b0188573886d89d21e5c3c6951e6b1f3e26a564dae31fd42b4e34030a1ada1c6e3f970ac20f0f72'
