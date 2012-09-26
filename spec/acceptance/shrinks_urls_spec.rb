ae019b7f15e9b08f27f7a68080d4bd12dd710034
Baraa Basata <bbasata@gmail.com>
2012-09-26 08:08:53 -0400

    Shrinking multiple URLs - failing spec

--- spec/acceptance/shrinks_urls_spec.rb
+++ spec/acceptance/shrinks_urls_spec.rb
@@ -19,15 +19,28 @@ describe "Shrinking URLs" do
     last_response.body
   end
 
-  Given(:long_url) { "https://github.com/bbasata/shrinkwrap/tree/walking-skeleton" }
-  When(:short_url) { shrink(long_url) }
-  
   context "Shrinking a URL" do
+    Given(:long_url) { "https://github.com/bbasata/shrinkwrap/tree/walking-skeleton" }
+    When(:short_url) { shrink(long_url) }
     Then { short_url.should == "http://shrinkwrap.herokuapp.com/shorturl" }
   end
 
   context "Unshrinking a URL" do   
+    Given(:long_url) { "https://github.com/bbasata/shrinkwrap/tree/walking-skeleton" }
+    Given(:short_url) { shrink(long_url) }
     When(:unshrunk_url) { unshrink(short_url) }
     Then { unshrunk_url.should == long_url }
+  end
+
+  context "Shrinking multiple URLs" do
+    Given(:short_urls) { [
+      shrink("https://github.com/bbasata/shrinkwrap/blob/master/Gemfile"),
+      shrink("https://github.com/bbasata/shrinkwrap/blob/master/Rakefile")
+    ] }
+    Then { short_urls.uniq.should have(2).distinct_urls }
+  end
+
+  context "Shrinking the same URL twice" do
+    pending "produces the same short URL"
   end
 end
