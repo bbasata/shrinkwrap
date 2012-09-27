2a421774c5b3979b76d829270d89a467b144c700
Baraa Basata <bbasata@gmail.com>
2012-09-26 23:25:59 -0400

    Removed 0 and 1 from the alphabet to avoid confusion with O and I

--- lib/number_encoder.rb
+++ lib/number_encoder.rb
@@ -1,8 +1,8 @@
 module NumberEncoder
-  ALPHABET = (0..9).to_a + ('A'..'Z').to_a
+  ALPHABET = ('0'..'9').to_a + ('A'..'Z').to_a - ['0', '1']
 
   def self.encode(number)
   	# Using the radix gem, convert to a base 'n' number, where 'n' is the size of ALPHABET
-  	number.b(ALPHABET).to_s
+  	number.zero? ? ALPHABET.first : number.b(ALPHABET).to_s
   end
 end
--- spec/number_encoder_spec.rb
+++ spec/number_encoder_spec.rb
@@ -8,4 +8,14 @@ describe NumberEncoder, '#encode' do
   it 'produces a lexicographically larger value for a larger number' do
   	NumberEncoder.encode(1_000_000_000).should be > NumberEncoder.encode(999_999_999)
   end
+
+  it 'uses the alphabet 0-9 and A-Z except the numbers 0 and 1, to avoid confusion with the letters O and I' do
+    exclusions = ['0', '1']
+	  expected_alphabet = ('0'..'9').to_a + ('A'..'Z').to_a - exclusions
+
+    first_encodings = (0...expected_alphabet.length).collect { |number| NumberEncoder.encode(number) }
+    alphabet = first_encodings.uniq
+    
+    alphabet.should == expected_alphabet
+  end
 end
