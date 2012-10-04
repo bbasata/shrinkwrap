70bbedb57e99fce2f4cb10223adda8ac4bd4e053
Baraa Basata <bbasata@gmail.com>
2012-10-04 00:01:14 -0400

    Clarified NumberEncoder comment and converted tabs to spaces

--- lib/number_encoder.rb
+++ lib/number_encoder.rb
@@ -2,10 +2,11 @@ module NumberEncoder
   ALPHABET = ('0'..'9').to_a + ('A'..'Z').to_a - ['0', '1']
 
   def self.encode(number)
-  	# Using the radix gem, convert to a base 'n' number, where 'n' is the size of ALPHABET
-  	# Ensure that codes are sparese by the last two characters of the base 'n' number are 
-  	number = number * (ALPHABET.length + 1)
-  	number.zero? ? ALPHABET.first : number.b(ALPHABET).to_s
+    # Using the radix gem, convert to a base 'n' number, where 'n' is the size of ALPHABET
+    # Ensure that codes are sparse by ensuring the last two characters of the base 'n' number
+    # are different for encode(n) and encode(n + 1)
+    number = number * (ALPHABET.length + 1)
+    number.zero? ? ALPHABET.first : number.b(ALPHABET).to_s
   end
 
   def self.normalize(code)
