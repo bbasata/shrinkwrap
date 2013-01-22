59cf733cf0e86dbee1a7db58e855f8665557a86b
Baraa Basata <bbasata@gmail.com>
2013-01-21 22:52:14 -0500

    Refactor unique number seed

--- app/models/globally_unique_number.rb
+++ app/models/globally_unique_number.rb
@@ -1,14 +1,14 @@
 class GloballyUniqueNumber < ActiveRecord::Base
   def self.next
-    number = first
+    number = first || seed
     number.last_value = number.last_value + 1
     number.save
     number.last_value
   end
 
   def self.seed
-    number = new
-    number.last_value = 1
-    number.save
+    number = new.tap do |instance|
+      instance.last_value = 1
+    end
   end
 end
